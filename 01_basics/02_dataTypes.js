1b706906aeca98479bf3aadc9b424562c043486b
whitejr0 <123718424+whitejr0@users.noreply.github.com>
2024-10-16 08:48:24 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js nor browser
+
+console.log(3+3);
+console.log("Sid");
+
+let name = "Sid"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
